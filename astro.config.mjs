2fa02003e50005fa91ec0656178b97e988b93dd2
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2023-07-24 19:47:11 +1000

    for github io

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: './docs',
+  server: { port: 3333, host: true },
   integrations: [tailwind()]
 });
