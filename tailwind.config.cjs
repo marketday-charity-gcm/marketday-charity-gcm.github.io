34bf02830f8606f9f9bb923649545d7fef4558db
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2023-07-25 00:09:53 +1000

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
 		extend: {
       fontFamily: {
         'script': ['Dancing Script', 'cursive'],
