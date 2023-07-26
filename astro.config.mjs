9dfc112a15803e9b98b44d0a63448d952965c199
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2023-07-26 21:53:32 +1000

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 3333, host: true },
+  server: {
+    port: 3333,
+    host: true
+  },
   site: 'https://marketday-charity-gcm.github.io/',
-  integrations: [tailwind(),sitemap()]
+  integrations: [tailwind(), sitemap()]
 });
